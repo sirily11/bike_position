69be81509e836b83b090e633d4e55d4bfe1546df
sirily11 <sirily1997@gmail.com>
2021-01-23 02:05:24 +0800

    Initial Commit

---
+++ bike_position/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bike_position
+//
+//  Created by 李其炜 on 1/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bike_position/bike_positionApp.swift
@@ -0,0 +1,17 @@
+//
+//  bike_positionApp.swift
+//  bike_position
+//
+//  Created by 李其炜 on 1/23/21.
+//
+
+import SwiftUI
+
+@main
+struct bike_positionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
